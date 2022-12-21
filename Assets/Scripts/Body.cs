fc2f1480377d4bcca8ea83cecf93500b3cc5731f
Luiz Strobelt <lhsperez@gmail.com>
2022-12-21 14:33:08 -0300

    Calculate mass and scale objects

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(Rigidbody))]
 public class Body : MonoBehaviour
 {
     public Vector3 Velocity = Vector3.zero;
 
-    private Color _planetColor;
-    private Rigidbody _rigidBody;
-    public float Mass { get; private set; }
+    public float Radius = 1f;
+    public float SurfaceGravity = 9.8f;
+    private Rigidbody _rb;
+
+    private Color _surfaceColor;
+    public float Mass => _rigidBody.mass;
+
+    private Rigidbody _rigidBody
+    {
+        get
+        {
+            if (!_rb) _rb = GetComponent<Rigidbody>();
+            return _rb;
+        }
+    }
 
     private void Awake()
     {
-        _planetColor = Random.ColorHSV();
-        GetComponent<MeshRenderer>().material.color = _planetColor;
-        _rigidBody = GetComponent<Rigidbody>();
-        Mass = _rigidBody.mass;
+        _surfaceColor = Random.ColorHSV();
+        GetComponent<MeshRenderer>().sharedMaterial.color = _surfaceColor;
+        UpdateMassAndScale();
     }
 
-    private void Update()
+    private void OnValidate()
     {
-        //transform.localScale.
+        UpdateMassAndScale();
+    }
+
+    private void UpdateMassAndScale()
+    {
+        _rigidBody.mass = SurfaceGravity * Radius * Radius / UniversalConstants.Gravity;
+        transform.localScale = Vector3.one * Radius;
     }
 
     public void ApplyForce(Vector3 acceleration)
@@ -28,7 +46,7 @@ public class Body : MonoBehaviour
         var previousPosition = _rigidBody.position;
         var newPosition = previousPosition + Velocity;
 
-        Debug.DrawLine(previousPosition, newPosition, _planetColor, 100000);
+        Debug.DrawLine(previousPosition, newPosition, _surfaceColor, 100000);
         _rigidBody.MovePosition(newPosition);
     }
 }
